51504ee5cd4d6afcfd7f1fb3ed813f2b728004b4
Ramom <ramonss.bque@gmail.com>
2016-08-25 13:34:22 -0300

    Atualizado arquivo index - Feito upload do pacote bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<body>
+    <form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Endereço de e-mail</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Senha</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputFile">Entrada de arquivo</label>
+        <input type="file" id="exampleInputFile">
+        <p class="help-block">Exemplo de texto de ajuda em nível de bloco.</p>
+    </div>
+    <div class="checkbox">
+        <label>
+        <input type="checkbox"> Me selecione
+        </label>
+    </div>
+    <button type="submit" class="btn btn-default">Enviar</button>
+    </form>
+</body>
 
+</html>
